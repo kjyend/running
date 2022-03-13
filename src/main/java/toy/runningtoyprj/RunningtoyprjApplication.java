305a08173b7cb51a26b12cc1ff833ccbae34ea1c
junyoung kim <adad568@naver.com>
2022-03-13 15:12:57 +0900

    first-commit

---
+++ src/main/java/toy/runningtoyprj/RunningtoyprjApplication.java
@@ -0,0 +1,13 @@
+package toy.runningtoyprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningtoyprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningtoyprjApplication.class, args);
+	}
+
+}
